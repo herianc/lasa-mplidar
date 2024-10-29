a138fb07a3670116df237e00fac21644de6b9a1d
Herian <herian.cavalcante@live.com>
2024-10-28 22:53:35 -0300

    Atualizando os requisitos do script

--- mplscripts.py
+++ mplscripts.py
@@ -2,7 +2,10 @@ import pandas as pd
 import numpy as np
 import xarray as xr
 import os
+import warnings 
+from rich.progress import Progress
 
+warnings.filterwarnings('ignore') 
 
 def particle_height(dataset: xr.Dataset, particle_value: int) -> tuple:
     '''
@@ -43,8 +46,6 @@ def extract_info_mpl(PATH: str) -> pd.DataFrame:
 
     extract_info_mpl('./home/mpl/2023')
     """
-    print('Extraindo as informações dos arquivos em:' + PATH + '...')
-    print('Por favor, aguarde!')
 
     # Listas das variáveis que serão extraídas
     data_hora = []
@@ -72,110 +73,118 @@ def extract_info_mpl(PATH: str) -> pd.DataFrame:
     mol_h_std = []
   
 
-    # For para percorrer a pasta com os dados .nc
-    for arquivo in os.listdir(PATH):
-        if arquivo.endswith('.nc'):
-            nome_arquivo = '/' + arquivo
-            try:
-                ds = xr.open_dataset(PATH + nome_arquivo)
+    # Barra de progresso para mostrar o andamento da extração
+    with Progress() as progress:
+        task = progress.add_task(f'Extraindo dados...', total=len(os.listdir(PATH)))
 
-                # Obtendo data e hora
-                data_str = ds['date_yyyyMMdd'].data[0]
-                hora_str = ds['time_hhmmss'].data[0]
-
-                # Extraindo a altura média dos aerossois
-                aero_media, aero_std = particle_height(ds, 5)
-                clean_aerossol_h_mean.append(aero_media)
-                clean_aerossol_h_std.append(aero_std)
+        # Extração dos dados 
+        for arquivo in os.listdir(PATH):
+            progress.update(task, advance=1)
             
-                pol_media, pol_std = particle_height(ds, 6)
-                pol_aerossol_h_mean.append(pol_media)
-                pol_aerossol_h_std.append(pol_std)
+            if arquivo.endswith('.nc'):
+                #print(f'Extraindo informações de: {arquivo}', end='\r')
+                nome_arquivo = '/' + arquivo
+                try:
+                    ds = xr.open_dataset(PATH + nome_arquivo)
+    
+                    # Obtendo data e hora
+                    data_str = arquivo.split('_')[2].split('.')[0]
+    
+                    # Extraindo a altura média dos aerossois
+                    aero_media, aero_std = particle_height(ds, 5)
+                    clean_aerossol_h_mean.append(aero_media)
+                    clean_aerossol_h_std.append(aero_std)
                 
-                ice_media, ice_std = particle_height(ds, 2)
-                ice_h_mean.append(ice_media)
-                ice_h_std.append(ice_std)
-                
-                mol_media, mol_std = particle_height(ds, 4)
-                mol_h_mean.append(mol_media)
-                mol_h_std.append(mol_std)
-                
-                wcloud_media, wcloud_std = particle_height(ds, 0)
-                wcloud_h_mean.append(wcloud_media)
-                wcloud_h_std.append(wcloud_std)
-                
-                mcloud_media, mcloud_std = particle_height(ds, 1)
-                mcloud_h_mean.append(mcloud_media)
-                mcloud_h_std.append(mcloud_std)
+                    pol_media, pol_std = particle_height(ds, 6)
+                    pol_aerossol_h_mean.append(pol_media)
+                    pol_aerossol_h_std.append(pol_std)
+                    
+                    ice_media, ice_std = particle_height(ds, 2)
+                    ice_h_mean.append(ice_media)
+                    ice_h_std.append(ice_std)
+                    
+                    mol_media, mol_std = particle_height(ds, 4)
+                    mol_h_mean.append(mol_media)
+                    mol_h_std.append(mol_std)
+                    
+                    wcloud_media, wcloud_std = particle_height(ds, 0)
+                    wcloud_h_mean.append(wcloud_media)
+                    wcloud_h_std.append(wcloud_std)
+                    
+                    mcloud_media, mcloud_std = particle_height(ds, 1)
+                    mcloud_h_mean.append(mcloud_media)
+                    mcloud_h_std.append(mcloud_std)
+    
+                    # Normalizando o AOD (Essa normalização é feita baseada na normalização do SigmaMPL)
+                    if ds['aod'].values[0] < 0:
+                        aod_value = 0
+                    elif ds['aod'].values[0] > 2:
+                        aod_value = 2
+                    else:
+                        aod_value = ds['aod'].values[0]
+    
+                    # Adicionando os dados as listas de variáveis
+                    clean_aerossol.append(ds['particle_type'].data[0].tolist().count(5))
+                    polluted_aerosol.append(ds['particle_type'].data[0].tolist().count(6))
+                    data_hora.append(data_str)
+                    clp.append(ds['pbls'].values[0][0]) # Altura da CLP
+                    aod.append(aod_value) # Valor da CLP 
+                    temp.append(ds['detector_temperature'].data[0])
+                    
+                    # Contagem de particulas 
+                    rain_dust.append(ds['particle_type'].data[0].tolist().count(3)) # Rain/Dust
+                    ice_dust_ash.append(ds['particle_type'].data[0].tolist().count(2)) # Ice/Dust/Ash
+                    molecular.append(ds['particle_type'].data[0].tolist().count(4)) # Molecular
+                    water_clouds.append(ds['particle_type'].data[0].tolist().count(0)) # Water Clouds
+                    mixed_clouds.append(ds['particle_type'].data[0].tolist().count(1)) # Mixed Clouds
 
-                # Normalizando o AOD
-                if ds['aod'].values[0] < 0:
-                    aod_value = 0
-                elif ds['aod'].values[0] > 2:
-                    aod_value = 2
-                else:
-                    aod_value = ds['aod'].values[0]
+                except Exception:
+                    print(f'Erro inesperado: Não foi possível extrair informações de {arquivo}')
+                    print(str(Exception))
+                    print('Extraindo informações dos demais arquivos')
+                    continue
 
-                # Adicionando os dados as listas de variáveis
-                clean_aerossol.append(ds['particle_type'].data[0].tolist().count(5))
-                polluted_aerosol.append(ds['particle_type'].data[0].tolist().count(6))
-                data_hora.append(data_str+' '+hora_str)
-                clp.append(ds['pbls'].values[0][0]) # Altura da CLP
-                aod.append(aod_value) # Valor da CLP 
-                temp.append(ds['detector_temperature'].data[0])
-                
-                # Contagem de particulas 
-                rain_dust.append(ds['particle_type'].data[0].tolist().count(3)) # Rain/Dust
-                ice_dust_ash.append(ds['particle_type'].data[0].tolist().count(2)) # Ice/Dust/Ash
-                molecular.append(ds['particle_type'].data[0].tolist().count(4)) # Molecular
-                water_clouds.append(ds['particle_type'].data[0].tolist().count(0)) # Water Clouds
-                mixed_clouds.append(ds['particle_type'].data[0].tolist().count(1)) # Mixed Clouds
-
-                # Criando um dataframe do pandas para agrupar os dados
-                df = pd.DataFrame({
-                    'data_hora': data_hora,
-                    'temperatura': temp,
-                    'aod': aod,
-                    'clp': clp,
-                    'clean_aerossol': clean_aerossol,
-                    'clean_aero_h_mean': clean_aerossol_h_mean,
-                    'clean_aero_h_std': clean_aerossol_h_std,
-                    'polluted_aerossol': polluted_aerosol,
-                    'pol_aero_h_mean': pol_aerossol_h_mean,
-                    'pol_aerossol_h_std': pol_aerossol_h_std,
-                    'water_cloud': water_clouds,
-                    'wcloud_h_mean': wcloud_h_mean,
-                    'wcloud_h_std':wcloud_h_std,
-                    'mixed_clouds': mixed_clouds,
-                    'mcloud_h_mean': mcloud_h_mean,
-                    'mcloud_h_std':mcloud_h_std,
-                    'molecular': molecular,
-                    'mol_h_mean':mol_h_mean,
-                    'mol_h_std':mol_h_std,
-                    'rain_dust': rain_dust,
-                    'ice_dust_ash': ice_dust_ash,
-                    'ice_h_mean':ice_h_mean,
-                    'ice_h_std':ice_h_std,
-                })
-
-                # Convertendo a coluna data_hora de str para datetime
-                df['data_hora'] = pd.to_datetime(df['data_hora'], format='%Y%m%d %H%M%S')
-
-                # Convertendo o horário de GMT para Local
-                df['data_hora'] = df['data_hora'] - pd.DateOffset(hours=3)
-
-                # Ordenando o dataframe pela data
-                df.sort_values('data_hora', inplace=True)
-                df.reset_index(inplace=True)
-                df.drop(columns=['index'], inplace=True)
-
-            except Exception:
-                print(f'Erro inesperado: Não foi possível extrair informações de {arquivo}')
-                print(Exception)
-                print('Extraindo informações dos demais arquivos')
-                continue
-
-    print('Encerrado')
+        if progress.finished:
+            print('Extração encerrada')
+        
+        # Criando um dataframe do pandas para agrupar os dados
+        df = pd.DataFrame({
+                        'data_hora': data_hora,
+                        'temperatura': temp,
+                        'aod': aod,
+                        'clp': clp,
+                        'clean_aerossol': clean_aerossol,
+                        'clean_aero_h_mean': clean_aerossol_h_mean,
+                        'clean_aero_h_std': clean_aerossol_h_std,
+                        'polluted_aerossol': polluted_aerosol,
+                        'pol_aero_h_mean': pol_aerossol_h_mean,
+                        'pol_aerossol_h_std': pol_aerossol_h_std,
+                        'water_cloud': water_clouds,
+                        'wcloud_h_mean': wcloud_h_mean,
+                        'wcloud_h_std':wcloud_h_std,
+                        'mixed_clouds': mixed_clouds,
+                        'mcloud_h_mean': mcloud_h_mean,
+                        'mcloud_h_std':mcloud_h_std,
+                        'molecular': molecular,
+                        'mol_h_mean':mol_h_mean,
+                        'mol_h_std':mol_h_std,
+                        'rain_dust': rain_dust,
+                        'ice_dust_ash': ice_dust_ash,
+                        'ice_h_mean':ice_h_mean,
+                        'ice_h_std':ice_h_std,
+                    })
+    
+        # Convertendo a coluna data_hora de str para datetime
+        df['data_hora'] = pd.to_datetime(df['data_hora'], format='%Y%m%d%H%M')
+    
+        # Convertendo o horário de GMT para Local
+        df['data_hora'] = df['data_hora'] - pd.DateOffset(hours=3)
+    
+        # Ordenando o dataframe pela data
+        df.sort_values('data_hora', inplace=True)
+        df.reset_index(inplace=True)
+        df.drop(columns=['index'], inplace=True)
+    
     return df 
 
 
@@ -256,10 +265,7 @@ def extract_cloud_height(PATH:str) -> pd.DataFrame:
                 df.drop(columns=['index'], inplace=True)
 
             except Exception:
-                print(f'Erro inesperado: Não foi possível extrair informações de {arquivo}')
-                print(Exception)
-                print('Extraindo informações dos demais arquivos')
                 continue
 
-    print('Encerrado')
-    return df 
+    print('\nEncerrado')
+    return df  
